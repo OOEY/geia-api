32c0b89eb8a725c945f02c708fe1edaa27d7899a
bald <jessjeanjo@gmail.com>
2017-03-10 19:14:34 -0500

    add simple backend

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
 #!flask/bin/python
 
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return "Hello, you fucker!"
+ip = [
+	{
+		'id': 1,
+		'name': 'some insurance provider',
+		'address': '9818 flatlands ave',
+		'number': 9175898478,
+		'services': ['sex', 'face']
+			
+	},
+	{
+		'id': 2,
+		'name': 'some insurance other  provider',
+		'address': '9820 flatlands ave',
+		'number': 9175848478,
+		'services': ['sex', 'face']
+			
+	}
+]
+
+
+@app.route('/geia/api/v1.0/ip', methods=['GET'])
+def get_ip():
+	return jsonify({'providers': ip})
+
 
 
 if __name__ == '__main__':
